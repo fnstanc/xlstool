7c2d69cd91c187e703bd5b08893c08a5e7a59e42
fnstanc <fn.stanc@gmail.com>
2018-07-14 12:00:51 +0800

    Hello world.

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# File Name: run.sh
+# Author: Stan.Lch
+# mail: fn.stanc@gmail.com
+# Created Time: 2018/7/12 13:15:37
+
+export PATH=$PATH:./tool
+python ./src/xlstool.py -g c ./example
